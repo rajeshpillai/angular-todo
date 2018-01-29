1a2bc91bbe7c74b9d3afc0e1686446f10931430b
rajesh.pillai <rajesh.pillai@kohls.com>
2018-01-28 22:23:12 -0800

    Added add and delete.  Edit in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,26 +9,42 @@ export class AppComponent {
   title = 'Angular TODO App';
 
   todos = [
-    {id: 1, title: "Learn Angular", completed: false},
-    {id: 2, title: "Learn fundamental React", completed: true},
-    {id: 3, title: "Lear basic HapiJS", completed: false}
+    {id: 1, title: "Learn Angular", completed: false, edit: false},
+    {id: 2, title: "Learn fundamental React", completed: true, edit: false},
+    {id: 3, title: "Lear basic HapiJS", completed: false, edit: false}
   ];
 
   addTodo(newTodo: HTMLInputElement) {
     var todo = {
       id: this.todos.length + 1,
       title: newTodo.value,
-      completed: false
+      completed: false,
+      edit: false
     }
-
     this.todos.push(todo);
-
     this.log(this.todos);
-
     return false;
   }
 
-  log (o) {
-    console.log(JSON.stringify(o));
+  deleteTodo(id) {
+    var newTodos = this.todos.filter ((todo) => {
+      return todo.id !=id;
+    });
+
+    this.todos = newTodos;
+  }
+
+  toggleEdit(id) {
+    var todoEdit = this.todos.find((todo) => {
+      return todo.id == id;
+    });
+    todoEdit.edit = !todoEdit.edit;
+    this.log ("edit: ", todoEdit);
+  }
+
+  log (...args) {
+    for(var i = 0; i < args.length; i++) {
+     console.log(JSON.stringify(args[i]));
+    }
   }
 }
