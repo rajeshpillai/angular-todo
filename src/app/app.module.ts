cdc1ea90a9b7d0fa359d664842090451c226619b
rajesh.pillai <rajesh.pillai@kohls.com>
2018-01-29 02:43:46 -0800

    Feature: Basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './feature/about/about.component';
+import { TodolistComponent } from './feature/todolist/todolist.component';
 
+const appRoutes: Routes = [
+  { path: '', component: TodolistComponent },
+  { path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    TodolistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
