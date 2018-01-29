229853ab2809bf4e908a263e978de7f96c03a0e8
rajesh.pillai <rajesh.pillai@kohls.com>
2018-01-29 05:32:40 -0800

    Fix: Bug

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -6,17 +6,6 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
-  animations: [
-    trigger('dialog', [
-      transition('void => *', [
-        style({ transform: 'scale3d(.3, .3, .3)' }),
-        animate(100)
-      ]),
-      transition('* => void', [
-        animate(100, style({ transform: 'scale3d(.0, .0, .0)' }))
-      ])
-    ])
-  ]
 })
 export class ModalComponent implements OnInit {
   @Input() closable = true;
